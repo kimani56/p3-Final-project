97dabf39e4a291c008968d96c25f6a7e11475282
gift kimani <gift.kimani@students.moringaschool.com>
2023-09-08 09:32:51 +0300

    connection

--- app/connect.py
+++ app/connect.py
@@ -5,4 +5,4 @@ from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = 'sqlite:///student_management.db'
 engine = create_engine(DATABASE_URL)
-Session = sessionmaker(bind=engine)
+Session = sessionmaker(bind=engine) 
